f8551573b3cd256869818c2afcb1a44a63eaa45e
David S Grigoryan <me@davidsergey.com>
2024-11-19 17:06:57 +0000

    Changed view port of the Fyuld Storyboard

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export async function init() {
   miro.board.ui.on('icon:click', async () => {
-    await miro.board.ui.openPanel({url: 'app.html'})
-    // await miro.board.ui.openModal({url: 'app.html', width: 1280, height: 720})
+    // await miro.board.ui.openPanel({url: 'app.html'})
+    await miro.board.ui.openModal({url: 'app.html', width: 1280, height: 720})
   });
 }
 
